633b1e01e86365ab881889d87adf79c6018d4e0f
luoshuhong <luoshuhong.jian@foxmail.com>
2016-12-09 21:03:15 +0800

    submit

---
+++ src/main/java/com/zunyiv/admin/WeiBoTest.java
@@ -0,0 +1,34 @@
+package com.zunyiv.admin;
+
+import org.apache.commons.logging.Log;
+import weibo4j.Oauth;
+import weibo4j.model.WeiboException;
+import weibo4j.util.BareBonesBrowserLaunch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Administrator on 2016/12/9.
+ */
+public class WeiBoTest {
+    public static void main(String [] args) throws WeiboException, IOException {
+        Oauth oauth = new Oauth();
+        BareBonesBrowserLaunch.openURL(oauth.authorize("code"));
+        System.out.println(oauth.authorize("code"));
+        System.out.print("Hit enter when it's done.[Enter]:");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String code = br.readLine();
+//        Log.logInfo("code: " + code);
+        try{
+            System.out.println(oauth.getAccessTokenByCode(code));
+        } catch (WeiboException e) {
+            if(401 == e.getStatusCode()){
+//                Log.logInfo("Unable to get the access token.");
+            }else{
+                e.printStackTrace();
+            }
+        }
+    }
+}
